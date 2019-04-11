844f0b30edec1c8b3eb984da59305c0b4df7509d
Antonio Romito <antonio.romito@manicom.io>
2019-04-11 10:41:21 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rhsm-api-client',
+    version='1.0',
+    packages=['', 'rhsm'],
+    url='https://github.com/antonioromito/rhsm-api-client',
+    license='GPLv2+',
+    author='Antonio Romito',
+    author_email='aromito@redhat.com',
+    description='Red Hat Subscription Manager (RHSM) APIs client interface to collect a data from your RHSM account. '
+)
