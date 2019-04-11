73ccca98e94e0e8a60867e3d2a9149d8fcd904be
Antonio Romito <antonio.romito@manicom.io>
2019-04-11 14:41:02 +0200

    created __run__py

---
+++ __run__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# This file is part of the sos project: https://github.com/antonioromito/rhsm-api-client
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+""" rhsm-api-client entry point. """
+
+try:
+    from rhsm.RHSMClient import main
+except KeyboardInterrupt:
+    raise SystemExit()
+
+if __name__ == '__main__':
+    main()
