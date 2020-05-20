e23a00054c587d5a675ce9683bfdca187cf0075b
Antonio Romito <antonio.romito@manicom.io>
2020-05-20 18:30:41 +0200

    * fixing setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from rhsm import __version__ as VERSION
 setup(
     name='rhsm',
     version=VERSION,
-    description='Red Hat Subscription Manager (RHSM) APIs client interface to collect a data '
+    description='Red Hat Subscription Manager (RHSM) APIs client interface to collect data '
                 'from your RHSM account. ',
     author='Antonio Romito',
     author_email='aromito@redhat.com',
     url='https://github.com/antonioromito/rhsm-api-client',
     license='GPLv2+',
     scripts=['rhsm-cli'],
-    packages=['rhsm', 'rhsm.objects', 'rhsm.formats']
+    packages=['rhsm', 'rhsm.objects', 'rhsm.formats'],
+    install_requires=['oauthlib', 'requests']
 )
